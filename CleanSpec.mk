5bbf300025f95dec3ef2e5c551b5a5bd69904c59
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2023-02-08 13:10:54 +0100

    prebuilts: Update cleanspec
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Change-Id: Ib43ec6c0438c24d92b487cd926d0ede75cad5b0c

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -1,2 +1,6 @@
-$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/MotoCalculator)
-$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/DuckDuckGo)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/product/priv-app/MotoCalculator)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/product/priv-app/DuckDuckGo)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/product/priv-app/Quickpic)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/ViaBrowser)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/Superiorwalls)
+
